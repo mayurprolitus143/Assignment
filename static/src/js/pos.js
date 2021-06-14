8ddd6f0bfd9ed8d1e71b4bbe6b34e1d8ec80c039
mayur <mayur.kumar@prolitus.com>
2021-06-14 11:20:23 +0530

    First Commit

---
+++ static/src/js/pos.js
@@ -0,0 +1,8 @@
+odoo.define('POS_Application.pos', function (require) {
+  "use strict";
+    console.log("This is PoS.js File  Console")
+
+    var model = require("point_of_sale.models");
+    model.load_fields('res.partner',['adhaar_id','voter_id','mobile']);
+
+});
